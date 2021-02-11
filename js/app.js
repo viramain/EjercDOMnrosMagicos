6876e6f5b3642e085288d9f9adfbecb17bd56184
Valentina Iramain <valentinairamain2020@gmail.com>
2021-02-11 12:32:14 -0300

    Ejercicio terminado

--- js/app.js
+++ js/app.js
@@ -1,3 +1,12 @@
+// verifica que el numero ingresado este entre 0 y 100
+function validaNro(numero) {
+    if (numero.value.trim() != "" && !isNaN(numero.value) && numero.value >= 0 && numero.value <= 100) {
+        numero.className = "form-control is-valid";
+    } else {
+        numero.className = "form-control is-invalid";
+    }
+}
+
 //Genera numero aleatorio entre 0 y 100 (NUMERO MAGICO)
 function generaNro(min, max) {
     min = Math.ceil(min);
@@ -10,19 +19,18 @@ function comparaNros() {
     let ingresado = document.getElementById("ingrNro ").value
     let aleatorio = generaNro(0, 100);
     imprimeResultado(aleatorio, ingresado);
-    // poner focus en boton COMENZAR???
-    // document.getElementById("botonComenzar ").focus();
+    // Cada vez que termina el juego, poner focus en boton COMENZAR
+    document.getElementById("botonComenzar").focus();
 }
 
 // Ir al campo para ingresar numero
 function getFocus() {
-    // document.getElementById("ingrNro ").focus();
     // inicializa en cero el numero a ingresar
     document.getElementById("ingrNro ").value = 0;
-    // vacia el mensaje del resultado
-    borraResultado();
     // pone el cursor en el campo para ingresar numero
     document.getElementById("ingrNro ").select();
+    // vacia el mensaje del resultado
+    borraResultado();
 }
 
 // Imprime resultado
